13cbeb67edb95881964d34eca720b31a35582573
YuYing_Liang <yuying.liang00@gmail.com>
2023-03-15 13:17:03 -0400

    added code for nonlinear controller

--- adaptive-model-predictive/controller/calc_adaptive_control.m
+++ adaptive-model-predictive/controller/calc_adaptive_control.m
@@ -1,6 +1,8 @@
 function [f] = calc_adaptive_control(A, b, C, rho, x_k, u, q, r, Hp, Hu, k)
     f=0;
     for i=1:Hp
+        % this is the estimated model (from calc_p_star)
+        % we pass in u to f_star which is the vector we want to minimize
         x_k = A*x_k + b*u(i);
         z = C*x_k;
         if k+i <= length(rho(1,1,:))
--- adaptive-model-predictive/controller/calc_p_star.m
+++ adaptive-model-predictive/controller/calc_p_star.m
@@ -1,14 +1,24 @@
-function [f] = calc_p_star(A,b,C,x_init,rho,mn,u,k)
+function [f] = calc_p_star(A_star, A_actual, b, C, x_init, mn, u, k)
     f=0;
     w=[1,1,1];
     x_k = x_init;
+    x_k_obs = x_init;
+    
     for i=1:k
-        x_k = A*x_k + b*u(i);
+        % prediction
+        % estimated parameters go in here
+        x_k = A_star*x_k + b*u(i);
         y_k = C*x_k;
-        y_obs = rho(:,:,i) + mn(:,:,i);
+        
+        % true system
+        % true parameters that the controller doesn't know
+        x_k_obs = A_actual*x_k_obs + b*u(i);
+        y_k_obs = C*x_k_obs + mn(:,:,i);
+        
         for j=1:length(y_k)
-            f = f + ((y_k(j,1) - y_obs(j,1))/w(j))^2;
+            f = f + ((y_k(j,1) - y_k_obs(j,1))/w(j))^2;
         end
     end
+    
     f = (1/k)*f;
 end
